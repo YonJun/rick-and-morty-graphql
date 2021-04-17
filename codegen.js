066c3d2cdc4d05729f36f1a22b31bd4345d8498c
YonJun <jhon_08_2007@hotmail.com>
2021-04-17 00:18:09 -0500

    generate with codegen.js

--- codegen.js
+++ codegen.js
@@ -8,6 +8,10 @@ module.exports = {
         "typescript-operations",
         "typescript-react-apollo",
       ],
+      config: {
+        avoidOptionals: true,
+        maybeValue: "T",
+      },
     },
     "./src/__generated__/graphql.schema.json": {
       plugins: ["introspection"],
