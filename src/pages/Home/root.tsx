469293a952caa1ea011ab9113353b1867a78daec
YonJun <jhon_08_2007@hotmail.com>
2021-04-17 20:45:12 -0500

    update ui

--- src/pages/Home/root.tsx
+++ src/pages/Home/root.tsx
@@ -7,13 +7,15 @@ const Home: React.FC<HomeProps> = () => {
   return (
     <div className="bg-green-700 m-auto max-w-screen-lg h-auto min-h-screen ">
       <div className="bg-green-700 grid grid-cols-12 gap-5">
-        <div className="bg-green-500 col-span-8 flex items-end">
-          <div className="bg-red-700 w-full">
-            <Description />
+        <div className="grid grid-cols-12  col-span-12">
+          <div className="bg-green-500 col-span-8 flex items-end">
+            <div className="bg-red-700 w-full text-right">
+              <Description />
+            </div>
           </div>
-        </div>
-        <div className="bg-red-600 col-span-4">
-          <Character />
+          <div className="bg-red-600 col-span-4">
+            <Character />
+          </div>
         </div>
         <div className="bg-red-600 col-span-12">
           <List />
