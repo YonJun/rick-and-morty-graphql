abef374b38e701326dbaeb4bcb630a463f02f9df
YonJun <jhon_08_2007@hotmail.com>
2021-04-17 21:11:04 -0500

    grid container center

--- src/pages/Home/root.tsx
+++ src/pages/Home/root.tsx
@@ -5,8 +5,8 @@ import List from "./layouts/List";
 interface HomeProps {}
 const Home: React.FC<HomeProps> = () => {
   return (
-    <div className="bg-green-700 m-auto max-w-screen-lg h-auto min-h-screen ">
-      <div className="bg-green-700 grid grid-cols-12 gap-5">
+    <div className="bg-green-700 m-auto max-w-screen-lg h-auto min-h-screen flex items-center">
+      <div className="bg-green-700 grid grid-cols-12 gap-5 w-full">
         <div className="grid grid-cols-12  col-span-12">
           <div className="bg-green-500 col-span-8 flex items-end">
             <div className="bg-red-700 w-full text-right">
